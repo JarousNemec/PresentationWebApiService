ff1d5cf8e03aa0902060c041e023d05e1d3f9836
JarousNemec <jarous.nemec@gmail.com>
2022-01-18 10:16:05 +0100

    added solarpanel state setting api with url state property

--- api.go
+++ api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -19,17 +20,18 @@ const (
 	account      = "minefinderstorageaccount"
 	key          = "vqgU7HTQ39+i05unpYXB2/sjw6YVBVCzcgbDlcq1UH04qw2V8TxKMOoaobMkjUuM587C/0NZjbtdBobd83rhGg=="
 	fullmetadata = "application/json;odata=fullmetadata"
-	tablename    = "MineFinder"
+	mftablename  = "MineFinder"
+	sptablename  = "solarpanels"
 )
 
-func insert(player string, playtime string, fieldSize string, mineCount string) {
+func insertGameResult(player string, playtime string, fieldSize string, mineCount string) {
 	client, err := storage.NewBasicClient(account, key)
 	if err != nil {
 		fmt.Printf("%s: \n", err)
 	}
 	tableCli = client.GetTableService()
 	fmt.Println(tableCli)
-	table := tableCli.GetTableReference(tablename)
+	table := tableCli.GetTableReference(mftablename)
 	entity := table.GetEntityReference("1", time.Now().String())
 	props := map[string]interface{}{
 		"player":    player,
@@ -53,7 +55,7 @@ func allGameResults(c *gin.Context) {
 	}
 	tableCli = client.GetTableService()
 	fmt.Println(tableCli)
-	table := tableCli.GetTableReference(tablename)
+	table := tableCli.GetTableReference(mftablename)
 	entities, err := table.QueryEntities(30, fullmetadata, nil)
 	var messages []byte
 
@@ -64,41 +66,79 @@ func allGameResults(c *gin.Context) {
 		fmt.Println(err)
 	}
 	c.JSON(http.StatusOK, entities.Entities)
-
 }
 
-//type T struct {
-//	PartitionKey string `json:"PartitionKey"`
-//	RowKey       string `json:"RowKey"`
-//	FieldSize    string `json:"fieldSize"`
-//	MineCount    string `json:"mineCount"`
-//	PlayTime     string `json:"playTime"`
-//	Player       string `json:"player"`
-//}
-//
-//func data(c *gin.Context) {
-//	var datas []T
-//	obj := T{PartitionKey: "buhehe", RowKey: "key", FieldSize: "5", MineCount: "5"}
-//	datas = append(datas, obj)
-//
-//	c.JSON(http.StatusOK, datas)
-//}
-
-func addResult(c *gin.Context) {
+func addGameResult(c *gin.Context) {
 	player := c.Request.URL.Query()["player"][0]
 	playtime := c.Request.URL.Query()["playtime"][0]
 	fieldSize := c.Request.URL.Query()["fieldSize"][0]
 	mineCount := c.Request.URL.Query()["mineCount"][0]
 	fmt.Println("inserting")
-	insert(player, playtime, fieldSize, mineCount)
+	insertGameResult(player, playtime, fieldSize, mineCount)
+}
+
+func insertSpState(state bool) {
+	client, err := storage.NewBasicClient(account, key)
+	if err != nil {
+		fmt.Printf("%s: \n", err)
+	}
+	tableCli = client.GetTableService()
+	fmt.Println(tableCli)
+	table := tableCli.GetTableReference(sptablename)
+	entity := table.GetEntityReference("1", "1")
+	props := map[string]interface{}{
+		"state": state,
+	}
+	entity.Properties = props
+	err = entity.Update(true, nil)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Updated! ")
+	}
+}
+
+func getSolarPanelState(c *gin.Context) {
+	client, err := storage.NewBasicClient(account, key)
+	if err != nil {
+		fmt.Printf("%s: \n", err)
+	}
+	tableCli = client.GetTableService()
+	fmt.Println(tableCli)
+	table := tableCli.GetTableReference(sptablename)
+	entities, err := table.QueryEntities(30, fullmetadata, nil)
+	var messages []byte
+
+	messages, err = json.Marshal(entities.Entities)
+	var mess string
+	err = json.Unmarshal(messages, &mess)
+	if err != nil {
+		fmt.Println(err)
+	}
+	c.JSON(http.StatusOK, entities.Entities)
+}
+
+func setSolarPanelsState(c *gin.Context) {
+	//TODO: nastavení stavu panelů; vymyslet způsob na bezpečný call, nemohl zavolat nikdo jiný
+	fmt.Println("setting spstate...")
+	data := c.Request.URL.Query()["state"][0]
+	strings.ToLower(data)
+	state := true
+	if data == "true" {
+		state = true
+	}
+	if data == "false" {
+		state = false
+	}
+	insertSpState(state)
 }
 
 func handleRequests() {
 	r := gin.Default()
-	//r.GET("/data.json", data)
-	r.GET("/addresult", addResult)
+	r.GET("/addresult", addGameResult)
 	r.GET("/allresults", allGameResults)
-	fmt.Println("prepare inserting")
+	r.GET("/setspstate", setSolarPanelsState)
+	r.GET("/spstate", getSolarPanelState)
 	r.Use(static.Serve("/", static.LocalFile("./frontend", false)))
 	err := r.Run(":8081")
 	if err != nil {
@@ -108,5 +148,4 @@ func handleRequests() {
 
 func main() {
 	handleRequests()
-
 }
